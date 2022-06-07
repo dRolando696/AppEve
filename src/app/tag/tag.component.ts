593978200d0a3f24b20e94d451cd493e3fc66bf6
Rolando <werecb@gmail.com>
2022-06-07 18:47:48 -0500

    ARRAY X6

--- src/app/tag/tag.component.ts
+++ src/app/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tag',
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagComponent implements OnInit {
 
-  public image:string="https://thepcenthusiast.com/wp-content/uploads/2021/01/msi-rtx-3080-suprim-x-review-17.jpg";
+  public image?:string;
   public Titulo:string="INTERPOLACION";
+  @Input() dataEntrante:any;
   constructor() { }
 
   ngOnInit(): void {
+    this.image = "https://thepcenthusiast.com/wp-content/uploads/2021/01/msi-rtx-3080-suprim-x-review-17.jpg"
   }
 
 }
